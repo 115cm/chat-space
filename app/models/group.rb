b08086906d5f8ea4481f11a4d2a19930d2adc039
115cm <59826145+115cm@users.noreply.github.com>
2020-01-22 19:14:30 +0900

    グループの作成/編集の機能実装

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,21 @@
 class Group < ApplicationRecord
   has_many :group_users
+  has_many :messages
   has_many :users, through: :group_users
+
   validates :name, presence: true, uniqueness: true
+  
+  # モデルにインスタンスメソッドを定義することで、ビューの記述をシンプルにすることができる。
+  def show_last_message
+    if (last_message = messages.last).present?  # 投稿の有無
+      if last_message.content?
+        last_message.content
+      else
+        "画像が投稿されています"
+      end
+    else
+      "まだメッセージはありません"
+    end
+  end
+
 end
